fc9244687365a0e814c01142189c83cf16a43a3f
Boniface Chacha <baxizzo@gmail.com>
2012-07-18 09:15:58 +0300

    added web context controller for web UI

---
+++ src/main/groovy/com/niafikra/meddela/Controller.groovy
@@ -0,0 +1,40 @@
+package com.niafikra.meddela
+
+import javax.servlet.ServletContextListener
+
+/**
+ *
+ * This act as a controller for meddella web application
+ * by managing when to start or stop meddella
+ *
+ * Author: Boniface Chacha <bonifacechacha@gmail.com>
+ * Date: 7/18/12
+ * Time: 8:12 AM
+ */
+class Controller implements ServletContextListener {
+
+    def static  meddela
+    def static appPath
+    def static appPathExt
+
+    @Override
+    void contextInitialized(javax.servlet.ServletContextEvent servletContextEvent) {
+
+        setPath(servletContextEvent.servletContext.getRealPath(""))
+         meddela=new meddela()
+        meddela.init()
+    }
+
+    void setPath(String path) {
+        appPath=path
+        appPathExt=  new StringBuilder(appPath)
+                .append(File.separator).append( "WEB-INF")
+                .append(File.separator).append("classes")
+                .toString();
+    }
+
+    @Override
+    void contextDestroyed(javax.servlet.ServletContextEvent servletContextEvent) {
+        meddela.stop()
+    }
+}
--- src/main/groovy/com/niafikra/meddela/meddela.groovy
+++ src/main/groovy/com/niafikra/meddela/meddela.groovy
@@ -1,10 +1,34 @@
 package com.niafikra.meddela
 
+import org.apache.log4j.Logger
+import org.apache.log4j.PropertyConfigurator
+
 /**
  * Author: Boniface Chacha <bonifacechacha@gmail.com>
  * Date: 7/17/12
  * Time: 2:09 PM
  */
-class meddela {
 
+class meddela {
+    private static Logger  logger=Logger.getLogger(meddela.class)
+
+    def init() {
+        initLog()
+        logger.info("meddella successsfully started ")
+    }
+
+    def initLog() {
+
+            String pathToSettings = new StringBuilder(Controller.getAppPathExt())
+                    .append(File.separator).append("config")
+                    .append(File.separator).append("log4j.properties")
+                    .toString();
+
+            PropertyConfigurator.configure(pathToSettings);
+
+    }
+
+    void stop() {
+        logger.info("meddella successsfully stopped ")
+    }
 }

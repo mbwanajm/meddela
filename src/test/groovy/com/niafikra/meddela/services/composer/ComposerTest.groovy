040471464f1631b6c9cb8dfb7ce34b7edbaf6274
mbwana.mbura <mbwanajm@gmail.com>
2012-07-19 18:50:21 +0300

    updated composer test to reflect changes

--- src/test/groovy/com/niafikra/meddela/services/composer/ComposerTest.groovy
+++ src/test/groovy/com/niafikra/meddela/services/composer/ComposerTest.groovy
@@ -1,6 +1,12 @@
 package com.niafikra.meddela.services.composer
 
 import spock.lang.Specification
+import com.niafikra.meddela.data.Template
+
+import com.niafikra.meddela.data.Notification
+import com.niafikra.meddela.services.transport.Transport
+import com.niafikra.meddela.services.transport.ConsoleTransport
+import com.niafikra.meddela.data.SentNotification
 
 /**
  * The composer specification
@@ -8,17 +14,17 @@ import spock.lang.Specification
  * Date: 19/07/12
  * Time: 14:39
  */
-class ComposerTest extends Specification{
+class ComposerTest extends Specification {
 
-    def "mergeResults feature"(){
+    def "mergeResults feature"() {
 
         given:
         def composer = new Composer()
         def results = [
-                [name:'mbwana', id:1],
-                [hobby:'programming', id: 1],
+                [name: 'mbwana', id: 1],
+                [hobby: 'programming', id: 1],
                 [name: 'boniface', id: 2],
-                [hobby: 'coding', id: 2],
+                [hobby: 'coding', id: 2]
         ]
 
         when: 'composer is called with an array of maps and given the key to use as the merging value'
@@ -28,8 +34,8 @@ class ComposerTest extends Specification{
         mergedResult.size() == 2
 
         and: 'the map contains a map of the merged properties with the mergingValue as the key'
-        mergedResult[1] == [name:'mbwana', id:1, hobby:'programming']
-        mergedResult[2] == [name:'boniface', id:2, hobby:'coding']
+        mergedResult[1] == [name: 'mbwana', id: 1, hobby: 'programming']
+        mergedResult[2] == [name: 'boniface', id: 2, hobby: 'coding']
 
     }
 

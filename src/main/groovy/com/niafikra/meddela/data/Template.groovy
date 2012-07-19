4f1b0d5aa8506f1417649181a382e7449cba45da
mbwana.mbura <mbwanajm@gmail.com>
2012-07-19 15:33:50 +0300

    renamed to SMSTemplate to template to make it more generic

--- src/main/groovy/com/niafikra/meddela/data/Template.groovy
+++ src/main/groovy/com/niafikra/meddela/data/Template.groovy
@@ -9,11 +9,11 @@ import java.util.Set;
  *         Date: 18/07/12
  *         Time: 17:34
  */
-public class SmsTemplate {
+public class Template {
     Set<String> sqls                // the set of SQL to use to get the data
     Set<String> groovyCodes         // groovy code that returns a collection of the required data
     String joiningProperty          // the common property used for joining results returned by different groovy or sql
-    String phoneNumberProperty      // the property to use as a phone number
+    String receiverProperty      // the property to use as a phone number
     String template                 // the template to use to compose the sms
 
 }

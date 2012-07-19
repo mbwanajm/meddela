acb3669aaf464ae5c67c73267dbc0cd452d8bc58
Boniface Chacha <baxizzo@gmail.com>
2012-07-19 18:40:46 +0300

    added logger to meddella

--- src/main/groovy/com/niafikra/meddela/meddela.groovy
+++ src/main/groovy/com/niafikra/meddela/meddela.groovy
@@ -1,5 +1,6 @@
 package com.niafikra.meddela
 
+import com.niafikra.meddela.auth.AuthenticationManager
 import org.apache.log4j.Logger
 import org.apache.log4j.PropertyConfigurator
 
@@ -8,27 +9,27 @@ import org.apache.log4j.PropertyConfigurator
  * Date: 7/17/12
  * Time: 2:09 PM
  */
-
 class meddela {
-    private static Logger  logger=Logger.getLogger(meddela.class)
+    private Logger logger = Logger.getLogger(meddela.class)
+
+    def authenticationManager
 
     def init() {
         initLog()
+        authenticationManager = new AuthenticationManager()
         logger.info("meddella successsfully started ")
     }
 
     def initLog() {
-
-            String pathToSettings = new StringBuilder(Controller.getAppPathExt())
-                    .append(File.separator).append("config")
-                    .append(File.separator).append("log4j.properties")
-                    .toString();
-
-            PropertyConfigurator.configure(pathToSettings);
-
+        String pathToSettings = new StringBuilder(Controller.getAppPathExt())
+                .append(File.separator).append("config")
+                .append(File.separator).append("log4j.properties")
+                .toString();
+        PropertyConfigurator.configure(pathToSettings);
     }
 
     void stop() {
         logger.info("meddella successsfully stopped ")
     }
+
 }

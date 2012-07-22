bfbd4371711f7dec4ede78c5d4568d6d0852b8c6
Boniface Chacha <baxizzo@gmail.com>
2012-07-22 12:26:50 +0300

    added comments

--- src/main/groovy/com/niafikra/meddela/ui/vaadin/dashboard/Dashboard.groovy
+++ src/main/groovy/com/niafikra/meddela/ui/vaadin/dashboard/Dashboard.groovy
@@ -1,58 +1,60 @@
 package com.niafikra.meddela.ui.vaadin.dashboard
 
-import com.vaadin.ui.Window
-import com.vaadin.ui.VerticalLayout
-import com.vaadin.ui.TabSheet
-import com.vaadin.ui.Component
 import com.niafikra.meddela.ui.vaadin.UIManager
+import com.niafikra.meddela.ui.vaadin.dashboard.settings.SettingsView
+import com.vaadin.ui.Component
+import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.Window
 
 /**
+ * Base Vaadin UI for managing the task of the meddela app
+ *
  * Author: Boniface Chacha <bonifacechacha@gmail.com>
  * Date: 7/18/12
  * Time: 8:14 PM
  */
-class Dashboard extends Window{
+class Dashboard extends Window {
 
     Toolbar toolbar
     UIManager uiManager
-    Component settingsView,reportsView,homeView
+    Component settingsView, reportsView, homeView
     VerticalLayout mainLayout
 
     Dashboard(UIManager uiManager) {
-      this.uiManager=uiManager
-      setCaption("meddela dashboard")
-      setName("dashboard")
+        this.uiManager = uiManager
+        setCaption("meddela dashboard")
+        setName("dashboard")
     }
 
     def build() {
-        this.settingsView=new SettingsView();
-        reportsView=new ReportView()
-        homeView=new HomeView()
+        this.settingsView = new SettingsView();
+        reportsView = new ReportView()
+        homeView = new HomeView()
 
         this.toolbar = new Toolbar(this)
-        mainLayout=new VerticalLayout()
+        mainLayout = new VerticalLayout()
         mainLayout.setSizeFull();
         setContent(mainLayout)
         mainLayout.addComponent(toolbar)
         mainLayout.addComponent(homeView)
     }
 
-    def showSettingsView(){
+    def showSettingsView() {
         showView(settingsView)
     }
 
-    def showReportsView(){
+    def showReportsView() {
         showView(reportsView)
     }
 
-    def showHomeView(){
+    def showHomeView() {
         showView(homeView)
     }
 
-    def showView(Component view){
+    def showView(Component view) {
         mainLayout.removeComponent(mainLayout.getComponent(1))
         mainLayout.addComponent(view)
-        mainLayout.setExpandRatio(view,1)
+        mainLayout.setExpandRatio(view, 1)
     }
 
 }

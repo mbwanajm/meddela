8a0d0f0b6b837796a3353e6709369dc2dc73d378
Boniface Chacha <baxizzo@gmail.com>
2012-07-23 19:47:53 +0300

    enabled adding default users

--- src/main/groovy/com/niafikra/meddela/Controller.groovy
+++ src/main/groovy/com/niafikra/meddela/Controller.groovy
@@ -1,6 +1,10 @@
 package com.niafikra.meddela
 
+import com.niafikra.meddela.meddela as Meddela
+
+import com.niafikra.meddela.data.security.Authentication
 import javax.servlet.ServletContextListener
+import org.apache.log4j.Logger
 
 /**
  *
@@ -16,6 +20,7 @@ class Controller implements ServletContextListener {
     def static meddela
     def static appPath
     def static appPathExt
+    Logger log = Logger.getLogger(Controller)
 
     @Override
     void contextInitialized(javax.servlet.ServletContextEvent servletContextEvent) {
@@ -23,6 +28,19 @@ class Controller implements ServletContextListener {
         setPath(servletContextEvent.servletContext.getRealPath(""))
         meddela = new meddela()
         meddela.init(appPath)
+        addDefaultUsers()
+    }
+
+    void addDefaultUsers() {
+        if (!Meddela.authenticationManager.authExist("admin")) {
+            Authentication admin = new Authentication()
+            admin.setUsername("admin")
+            admin.setPassword("admin")
+            admin.setEnabled(true)
+            Meddela.authenticationManager.addAuthentication(admin)
+            log.info("Added default users")
+        }
+        else log.info("Default users exists")
     }
 
     void setPath(String path) {

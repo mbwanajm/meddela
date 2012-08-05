5f808a860644ea0c0a3fa5c817c73721473ab7c4
Boniface Chacha <baxizzo@gmail.com>
2012-08-05 11:26:56 +0300

    enabled capturing of uncaught error in vaadin application

--- vaadinUI/src/main/groovy/com/niafikra/meddela/ui/vaadin/meddelaVaadinApplication.groovy
+++ vaadinUI/src/main/groovy/com/niafikra/meddela/ui/vaadin/meddelaVaadinApplication.groovy
@@ -2,6 +2,9 @@ package com.niafikra.meddela.ui.vaadin;
 
 
 import com.vaadin.Application
+import com.vaadin.terminal.Terminal
+import com.vaadin.ui.Window
+import org.apache.log4j.Logger
 
 /**
  * The Application's "main" class
@@ -9,11 +12,19 @@ import com.vaadin.Application
 @SuppressWarnings("serial")
 public class meddelaVaadinApplication extends Application {
     private UIManager uiManager
+    private Logger logger=Logger.getLogger(meddelaVaadinApplication)
 
     @Override
     public void init() {
         uiManager = new UIManager(this);
         uiManager.showLogin();
+    }
+
+    @Override
+    void terminalError(Terminal.ErrorEvent event) {
+        getMainWindow().showNotification("Please review your request or contact administrator",
+                                        Window.Notification.TYPE_ERROR_MESSAGE)
+        logger.error("meddela has uncaught exception",event.throwable)
     }
 
 }

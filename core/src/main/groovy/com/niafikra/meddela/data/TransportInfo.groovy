6aa9fd019519936e98a12c50770a48f9a0897cdd
Boniface Chacha <baxizzo@gmail.com>
2012-08-06 15:47:37 +0300

    equals method for allowing comparison

--- core/src/main/groovy/com/niafikra/meddela/data/TransportInfo.groovy
+++ core/src/main/groovy/com/niafikra/meddela/data/TransportInfo.groovy
@@ -9,6 +9,24 @@ class TransportInfo {
     String name
     HashMap configurations = new HashMap()
 
+    boolean equals(o) {
+        if (o==null) return false
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+
+        TransportInfo that = (TransportInfo) o
+
+        if (name != that.name) return false
+
+        return true
+    }
+
+    int hashCode() {
+        return (name != null ? name.hashCode() : 0)
+    }
+
+
+
     String toString(){
         return name
     }

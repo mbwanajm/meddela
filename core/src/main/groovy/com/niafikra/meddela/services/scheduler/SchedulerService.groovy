781530d80974cc13f3c9cbb14e2a682a0d4a429f
mbwana.mbura <mbwanajm@gmail.com>
2012-09-27 23:38:31 +0300

    prevented notification from being automatically scheduled when they are disabled

--- core/src/main/groovy/com/niafikra/meddela/services/scheduler/SchedulerService.groovy
+++ core/src/main/groovy/com/niafikra/meddela/services/scheduler/SchedulerService.groovy
@@ -47,6 +47,7 @@ class SchedulerService {
      * @return true if succesfull, false otherwise
      */
     boolean scheduleNotification(Notification notification) {
+        if(!notification.isEnabled()) return
         try {
             TriggerCheckTask triggerCheckTask = new TriggerCheckTask();
             def id = scheduler.schedule(notification.trigger.schedule, triggerCheckTask)
